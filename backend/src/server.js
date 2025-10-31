300ece6d57adf1c58d1fb3dd723e1f2db2c64df9
thanhlam1906 <lam221291@gmail.com>
2025-10-31 13:22:13 +0700

    sua loi

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,36 @@
-import express from 'express'; 
-import taskRoute from './routes/tasksRoutes.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import cors from "cors"
+import express from "express";
+import taskRoute from "./routes/tasksRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
 dotenv.config();
+
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
+
+// middlewares
 app.use(express.json());
-app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 app.use("/api/tasks", taskRoute);
-connectDB().then(()=>{ // Start the server only after DB connection is established
-    app.listen(PORT,() =>{
-    console.log(`Server is running on port ${PORT}`);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server bắt đầu trên cổng ${PORT}`);
+  });
 });
-})
